66c1b1dc9cb516abcabc7572bcd372bff2e327b7
Dave Duprey <dave@what3words.com>
2024-09-03 15:04:09 +0100

    added api wrapper for convenience

--- Package.swift
+++ Package.swift
@@ -17,7 +17,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-      .package(url: "https://github.com/what3words/w3w-swift-design.git", "1.0.0" ..< "2.0.0"),
+      .package(url: "https://github.com/what3words/w3w-swift-wrapper.git", "4.0.0"..<"5.0.0"),
+      .package(url: "https://github.com/what3words/w3w-swift-design.git", "1.0.0" ..< "2.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -25,7 +26,8 @@ let package = Package(
         .target(
             name: "W3WSwiftComponentsOcr",
             dependencies: [
-              .product(name: "W3WSwiftDesign", package: "w3w-swift-design"),
+              .product(name: "W3WSwiftApi", package: "w3w-swift-wrapper"),
+              .product(name: "W3WSwiftDesign", package: "w3w-swift-design")
             ],
             resources: [.process("Resources")]
         ),
