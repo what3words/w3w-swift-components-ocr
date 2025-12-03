3abddbee8d2a090c04862f8c2a23d136d8848064
Kaley Nguyen <kaley.nguyen@what3words.com>
2025-12-03 14:09:13 +0700

    update

--- Sources/W3WSwiftComponentsOcr/Lib/Ocr/W3WOcrDataTypes.swift
+++ Sources/W3WSwiftComponentsOcr/Lib/Ocr/W3WOcrDataTypes.swift
@@ -128,9 +128,13 @@ public extension W3WRfcLanguageProtocol {
   /// convert W3WRfcLanguage to W3WOcrLanguage
   func toW3WOcrLanguage() -> W3WOcrLanguage? {
     guard let langCode, !langCode.isEmpty else { return nil }
-    return W3WOcrLanguage(name: LanguageUtils.getLanguageName(forLocale: langCode, inLocale: "en"),
-                          nativeName: LanguageUtils.getLanguageName(forLocale: langCode, inLocale: langCode),
-                          code: langCode)
+    if let language = self as? W3WRfcLanguage {
+      return W3WOcrLanguage(name: language.name , nativeName: language.nativeName, code: langCode)
+    } else {
+      return W3WOcrLanguage(name: LanguageUtils.getLanguageName(forLocale: langCode, inLocale: "en"),
+                            nativeName: LanguageUtils.getLanguageName(forLocale: langCode, inLocale: langCode),
+                            code: langCode)
+    }
   }
 }
 
