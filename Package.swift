c1e62dcced5da559b765141bcc7be2ddd0f5ff98
Au Nguyen <au.nguyen@what3words.com>
2025-12-09 14:47:42 +0700

    update package to staging

--- Package.swift
+++ Package.swift
@@ -17,11 +17,11 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-      .package(url: "https://github.com/what3words/w3w-swift-core.git", "1.0.0"..<"2.0.0"),
-      .package(url: "https://github.com/what3words/w3w-swift-design.git", "1.0.0" ..< "2.0.0"),
-      .package(url: "git@github.com:what3words/w3w-swift-design-swiftui.git", "1.0.0" ..< "2.0.0"),
+      .package(url: "https://github.com/what3words/w3w-swift-core.git", branch: "staging"),
+      .package(url: "https://github.com/what3words/w3w-swift-design.git", branch: "staging"),
+      .package(url: "git@github.com:what3words/w3w-swift-design-swiftui.git", branch: "staging"),
       .package(url: "git@github.com:w3w-internal/w3w-swift-app-events.git", branch: "staging"),
-      .package(url: "https://github.com/what3words/w3w-swift-presenters.git", "1.0.0" ..< "2.0.0"),
+      .package(url: "https://github.com/what3words/w3w-swift-presenters.git", branch: "staging"),
       .package(url: "git@github.com:w3w-internal/w3w-swift-app-types.git", branch: "staging")
     ],
     targets: [
