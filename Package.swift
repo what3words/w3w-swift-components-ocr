2997e66a45fe7146119f952bdb586b8adb8056dd
hoangta <hoang.ta@what3words.com>
2026-03-14 17:43:50 +0700

    update packages

--- Package.swift
+++ Package.swift
@@ -17,13 +17,11 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-      //.package(url: "https://github.com/what3words/w3w-swift-wrapper.git", "4.0.0"..<"5.0.0"),
-      .package(url: "https://github.com/what3words/w3w-swift-core.git", branch: "staging"),
       .package(url: "https://github.com/what3words/w3w-swift-design.git", branch: "staging"),
-      .package(url: "git@github.com:what3words/w3w-swift-design-swiftui.git", branch: "dev/v5.6.0"),
-      .package(url: "git@github.com:w3w-internal/w3w-swift-app-events.git", branch: "dev/v5.6.0"),
+      .package(url: "https://github.com/what3words/w3w-swift-design-swiftui.git", branch: "dev/v5.6.0"),
+      .package(url: "https://github.com/w3w-internal/w3w-swift-app-events.git", branch: "dev/v5.6.0"),
       .package(url: "https://github.com/what3words/w3w-swift-presenters.git", branch: "dev/v5.6.0"),
-      .package(url: "git@github.com:w3w-internal/w3w-swift-app-types.git", branch: "dev/v5.6.0")
+      .package(url: "https://github.com/w3w-internal/w3w-swift-app-types.git", branch: "dev/v5.6.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -31,8 +29,6 @@ let package = Package(
         .target(
             name: "W3WSwiftComponentsOcr",
             dependencies: [
-              //.product(name: "W3WSwiftApi", package: "w3w-swift-wrapper"),
-              .product(name: "W3WSwiftCore", package: "w3w-swift-core"),
               .product(name: "W3WSwiftDesign", package: "w3w-swift-design"),
               .product(name: "W3WSwiftDesignSwiftUI", package: "w3w-swift-design-swiftui"),
               .product(name: "W3WSwiftPresenters", package: "w3w-swift-presenters"),
